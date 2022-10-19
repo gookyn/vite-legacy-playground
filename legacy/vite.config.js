3496cabb2bb23d07de7ef48541b5c7e745a89918
Gookyn <liujinge.work@gmail.com>
2022-10-19 17:07:41 +0800

    chore: update comments

--- legacy/vite.config.js
+++ legacy/vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
     legacy({
       /**
        * 1. Try changing these values
-       * 2. Run the build command to see the dist directory
-       * 3. Run the preview command to see the actual loaded files in different versions of browsers
+       * 2. Run `pnpm build`, see the output files in dist directory
+       * 3. Run `pnpm preview`, see the actual loaded files in different versions of browsers
        */
       targets: ['chrome >= 55'],
       renderLegacyChunks: true,
