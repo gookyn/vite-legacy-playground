452a100e4690b619c72d32c3cd3c926b7745bb36
Gookyn <liujinge.work@gmail.com>
2022-10-19 18:55:40 +0800

    feat: build for low version browsers

--- legacy-react/vite.config.ts
+++ legacy-react/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  // Works on both development and production build environments
+  esbuild: {
+    // Configure this value when the browser version of the development environment is lower
+    target: 'chrome64',
+    include: /\.(js|ts|jsx|tsx)$/,
+  },
+  plugins: [
+    react(),
+    // For production build environments only
+    legacy({
+      /**
+       * 1. Try changing these values
+       * 2. Run `pnpm build`, see the output files in dist directory
+       * 3. Run `pnpm preview`, see the actual loaded files in different versions of browsers
+       */
+      targets: ['chrome >= 55'],
+      renderLegacyChunks: true,
+      modernPolyfills: true,
+    }),
+  ],
+});
